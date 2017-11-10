f893e52f80b70768d1c2dfdffdbbe028809fb9df
Santoxjon <jonsantox@gmail.com>
2017-11-10 13:32:39 +0100

    Mensaje

--- app/src/main/java/com/example/dm2/aplicacionconfragmentos/SubHeroe.java
+++ app/src/main/java/com/example/dm2/aplicacionconfragmentos/SubHeroe.java
@@ -5,4 +5,53 @@ package com.example.dm2.aplicacionconfragmentos;
  */
 
 public class SubHeroe {
+    private String nombre, nombreReal, subpoder, motivacion, archienemigo;
+
+    public SubHeroe(String nombre, String nombreReal, String subpoder, String motivacion, String archienemigo) {
+        this.nombre = nombre;
+        this.nombreReal = nombreReal;
+        this.subpoder = subpoder;
+        this.motivacion = motivacion;
+        this.archienemigo = archienemigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreReal() {
+        return nombreReal;
+    }
+
+    public void setNombreReal(String nombreReal) {
+        this.nombreReal = nombreReal;
+    }
+
+    public String getSubpoder() {
+        return subpoder;
+    }
+
+    public void setSubpoder(String subpoder) {
+        this.subpoder = subpoder;
+    }
+
+    public String getMotivacion() {
+        return motivacion;
+    }
+
+    public void setMotivacion(String motivacion) {
+        this.motivacion = motivacion;
+    }
+
+    public String getArchienemigo() {
+        return archienemigo;
+    }
+
+    public void setArchienemigo(String archienemigo) {
+        this.archienemigo = archienemigo;
+    }
 }
