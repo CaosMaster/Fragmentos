93c1c8c1c7a879da8e02009155981833f38ac10c
Santoxjon <jonsantox@gmail.com>
2017-11-13 13:53:01 +0100

    Mensaje

--- app/src/main/java/com/example/dm2/aplicacionconfragmentos/MainActivity.java
+++ app/src/main/java/com/example/dm2/aplicacionconfragmentos/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.dm2.aplicacionconfragmentos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.dm2.aplicacionconfragmentos.FragmentListado.SubHeroesListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SubHeroesListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentListado frgListado = (FragmentListado) getSupportFragmentManager().findFragmentById(R.id.FrgListado);
+        frgListado.setSubHeroesListener(this);
+
+    }
+
+    @Override
+    public void onSubheroeSeleccionado(SubHeroe sh) {
+        boolean haydetalle = (getSupportFragmentManager().findFragmentById(R.id.FrgDetalle)!=null);
+        if(haydetalle){
+            ((FragmentDetalle) getSupportFragmentManager().findFragmentById(R.id.FrgDetalle)).mostrarDetalle(sh.getSubpoder());
+        }
     }
 }
